c6e7b80511502851d5212009915945bdaaa34b5d
cpena256 <nothinbuthits512@gmail.com>
2021-05-16 20:40:46 -0400

    add skills segment

--- elevator-pitch/src/ProfileCards.js
+++ elevator-pitch/src/ProfileCards.js
@@ -9,15 +9,18 @@ const [people, setPeople ] = useState([
     {
         name: "Elon Musk",
         url: "https://images.barrons.com/im-335496?width=620&size=1.5",
+        skills: "Electric Cars | Spacecraft  ", 
 
     },
     {
         name: "Jack Dorsey",
         url: "https://image.cnbcfm.com/api/v1/image/105218543-GettyImages-459305148.jpg?v=1545153819",
+        skills: "Social Media | Web Development ",
     },
     {
         name: "Erlich Bachman",
         url: "https://i2.wp.com/www.nationalreview.com/wp-content/uploads/2017/05/silicon-valleys-erlich-bachman-capitalist-hero.jpg?fit=788%2C460&ssl=1",
+        skills: "Marketing | Sales ",
     },
 ]);
     // useEffect(() => {
@@ -42,6 +45,7 @@ const [people, setPeople ] = useState([
              >
                  <div style={{ backgroundImage: `url(${person.url})` }} className="card">
                     <h3>{person.name}</h3>              
+                    <h4>{person.skills}</h4>
                  </div>
              </TinderCard>
             ) )}
